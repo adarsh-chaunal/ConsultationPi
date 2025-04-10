53c54b13bf9dd0c24c71707277f1d9e3f7c63ffe
adarsh-chaunal <er.adarsh97@gmail.com>
2025-04-10 10:37:42 +0530

    Feat: Configured shadcn in frontend project.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,58 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  //NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  //NavigationMenuViewport,
+} from "src/components/ui/navigation-menu";
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      ConsultationPi
-    </h1>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.tsx</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <NavigationMenuLink>Link</NavigationMenuLink>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
   );
 }
 
 export default App;
+
+// import React from "react";
+// import logo from "./logo.svg";
+// import "./App.css";
+
+// function App() {
+//   return (
+//     <h1 className="text-3xl font-bold underline">
+//       ConsultationPi
+//     </h1>
+//     // <div className="App">
+//     //   <header className="App-header">
+//     //     <img src={logo} className="App-logo" alt="logo" />
+//     //     <p>
+//     //       Edit <code>src/App.tsx</code> and save to reload.
+//     //     </p>
+//     //     <a
+//     //       className="App-link"
+//     //       href="https://reactjs.org"
+//     //       target="_blank"
+//     //       rel="noopener noreferrer"
+//     //     >
+//     //       Learn React
+//     //     </a>
+//     //   </header>
+//     // </div>
+//   );
+// }
+
+// export default App;
